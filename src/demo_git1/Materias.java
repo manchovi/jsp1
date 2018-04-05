0e49c53a5b8db738f8a180915b09d7ca92f6a58c
Lic. Manuel Gámez <winmanuel07@gmail.com>
2018-04-05 08:36:52 -0600

    Adicione líneas de código a la clase Materias

--- src/demo_git1/Materias.java
+++ src/demo_git1/Materias.java
@@ -1,4 +1,12 @@
 package demo_git1;
+
+import java.util.Scanner;
+
 public class Materias {
-    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        System.out.println("Ingrese el nombre de la materia que cursa: ");
+        String mataria = leer.nextLine();
+        
+    }
 }
